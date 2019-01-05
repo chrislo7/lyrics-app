51d82dbf4995ff490d8c792871bea828f662f8b6
Chris <chrislo.7@live.com>
2019-01-05 17:43:09 -0500

    added tracks folder to store files regarding API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Tracks extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tracks</h1>
+        <p>Placeholder text lol</p>
+      </div>
+    );
+  }
+}
+
+export default Tracks;
