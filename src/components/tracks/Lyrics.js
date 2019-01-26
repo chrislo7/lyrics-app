403d409c2ac04f0906af84da6cde4b98b4640657
Chris <chrislo.7@live.com>
2019-01-26 18:22:58 -0500

    Finished lyrics page

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -13,7 +13,8 @@ class Lyrics extends Component {
     componentDidMount = () => {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
             .then(res => {
-                this.setState({ lyrics: res.data.message.body.lyrics });
+                let str = res.data.message.body.lyrics.lyrics_body;
+                this.setState({ lyrics: str });
                 
                 return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
             })
@@ -25,22 +26,40 @@ class Lyrics extends Component {
 
     render() {
         const { track, lyrics } = this.state;
-        
+        console.log( track );
         if ( 
             track === undefined || 
             lyrics === undefined || 
             Object.keys(track).length === 0 || 
             Object.keys(lyrics).length === 0
         ) { return <Spinner /> }
-
         else { 
             return (
                 <React.Fragment>
                     <Link to="/" className="btn btn-dark btn-sm mb-4">Back</Link>
                     <div className="card">
-                        <div className="card-header">
-                            {track.track_name} by <span className="text-secondary">{ track.artist_name }</span>
+                        <h5 className="card-header">
+                            {track.track_name} by {' '}
+                            <span className="text-secondary">{ track.artist_name }</span>
+                        </h5>
+                        <div className="card-text m-3" >
+                            { lyrics.split('\n').map(function(item, key) {
+                                return (
+                                    <span key={key}> {item}<br/> </span>
+                                )
+                                }) }
                         </div>
+                        <ul className="list-group mt-3">
+                            <li className="list-group-item">
+                                <strong>Album</strong>: { track.album_name }
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Genre</strong>: { track.primary_genres.music_genre_list[0].music_genre.music_genre_name }
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Release</strong>: { new Date(track.updated_time).toLocaleDateString('en-EN') }
+                            </li>
+                        </ul>
                     </div>
                 </React.Fragment>
             )
